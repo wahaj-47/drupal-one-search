59c2f56d16c6009bdebcc0e3752ec1bde5936ef9
Wahaj Hussain <wahajhssn@gmail.com>
2025-11-19 14:56:11 -0600

    Re-submit search on popstate event

--- js/one-search.js
+++ js/one-search.js
@@ -1,6 +1,7 @@
 (function ($, Drupal) {
     Drupal.behaviors.oneSearch = {
         attach: function (context, settings) {
+
             once('one-search-form', '#one-search', context).forEach((form) => {
                 $(form).on('submit', function (e) {
                     const { protocol, host, pathname } = location;
@@ -13,24 +14,42 @@
 
                     if (pathname === actionUrl.pathname) {
                         e.preventDefault();
-                        const url = new URL(location)
-                        url.searchParams.set(filterIdentifier, term)
-                        history.pushState({}, "", url)
+
+                        const url = new URL(location);
+                        url.searchParams.set(filterIdentifier, term);
+                        history.pushState({}, "", url);
                     }
 
-                    settings.one_search.searchTerm = term;
-
-                    const views = $('.one-search-target', context)
-
-                    views.each(function () {
-                        const view = $(this);
-                        const input = view.find(`[name="${filterIdentifier}"]`)
-                        const submit = view.find('.view-filters .form-submit')
-                        input.val(term);
-                        submit.click();
-                    })
+                    triggerViewsSearch(term, filterIdentifier, context);
                 });
             });
+
+            once('one-search-popstate', 'body', context).forEach(() => {
+                window.addEventListener("popstate", function () {
+                    console.log("POP STATE");
+
+                    const filterIdentifier = settings.one_search.filterIdentifier;
+                    const term = new URL(location.href).searchParams.get(filterIdentifier) || '';
+
+                    triggerViewsSearch(term, filterIdentifier, context);
+                });
+            });
+
+            function triggerViewsSearch(term, filterIdentifier, context) {
+                settings.one_search.searchTerm = term;
+
+                const views = $('.one-search-target', context);
+
+                views.each(function () {
+                    const view = $(this);
+                    const input = view.find(`[name="${filterIdentifier}"]`);
+                    const submit = view.find('.view-filters .form-submit');
+                    input.val(term);
+                    submit.click();
+                });
+
+                $('#one-search input[name="' + filterIdentifier + '"]').val(term);
+            }
         }
     };
 })(jQuery, Drupal);
