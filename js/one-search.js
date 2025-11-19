4267aa153b0f11fd5d521370c901c0530ee24ae4
Wahaj Hussain <wahajhssn@gmail.com>
2025-11-19 10:16:10 -0600

    Maintain history on search submit

--- js/one-search.js
+++ js/one-search.js
@@ -3,17 +3,21 @@
         attach: function (context, settings) {
             once('one-search-form', '#one-search', context).forEach((form) => {
                 $(form).on('submit', function (e) {
-                    const { protocol, host, pathname } = window.location;
+                    const { protocol, host, pathname } = location;
 
                     const action = settings.one_search.action;
                     const actionUrl = new URL(action, `${protocol}//${host}`);
 
-                    if (pathname === actionUrl.pathname) {
-                        e.preventDefault();
-                    }
-
                     const filterIdentifier = settings.one_search.filterIdentifier;
                     const term = $(form).find(`input[name="${filterIdentifier}"]`).val() || '';
+
+                    if (pathname === actionUrl.pathname) {
+                        e.preventDefault();
+                        const url = new URL(location)
+                        url.searchParams.set(filterIdentifier, term)
+                        history.pushState({}, "", url)
+                    }
+
                     settings.one_search.searchTerm = term;
 
                     const views = $('.one-search-target', context)
